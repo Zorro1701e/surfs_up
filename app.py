1566133cf1ef91c08d9d8384881cb47893f86964
Zorro1701e <98936157+Zorro1701e@users.noreply.github.com>
2022-04-23 18:07:28 -0700

    push

--- app.py
+++ app.py
@@ -41,3 +41,11 @@ def welcome():
 
 if __name__ =="__main__":
     app.run(debug=True)
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
